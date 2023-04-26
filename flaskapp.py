ee8ebac869536895d4191313ac9ad07d0beeb3e5
Spencer Merrow <smerrow@gmu.edu>
2023-04-26 17:18:37 -0400

    adjusted syntax
    Signed-off-by: Spencer Merrow <smerrow@gmu.edu>

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
-from dbconnector import dbResult # import database connector class from dbconnector.py
+from dbconnector import dbResult  # import database connector class from dbconnector.py
 
-results = dbResult() # instantiate object
+results = dbResult()  # instantiate object
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     test = 'HELLO'
     return render_template("graphiccard.html")
+
 
 if __name__ == '__main__':
     app.run(debug=1)
